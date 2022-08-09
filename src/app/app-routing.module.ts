3a8cd8f2f0990c33cb5584e2cbd9b746d9a549c2
Deepak kumar <deepsingh262002@gmail.com>
2022-08-09 17:08:31 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth-components/login/login.component';
+import { SigninComponent } from './auth-components/signin/signin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ThemeComponent } from './themes/theme/theme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:SigninComponent,
+  },
+  {
+    path:'signup',
+    component:LoginComponent,
+  },
+  {
+    path: '',
+    component: ThemeComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ]
+  },
+  
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeComponent } from './themes/theme/theme.component';
+import { HeaderComponent } from './themes/header/header.component';
+import { FooterComponent } from './themes/footer/footer.component';
+import { SidebarComponent } from './themes/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecentSalesComponent } from './recent-sales/recent-sales.component';
+import { LoginComponent } from './auth-components/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    HomeComponent,
+    RecentSalesComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
